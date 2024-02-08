48df636d62486fba6e7c2523d0b7afb8bcc4597a
qdy <qdyqiudeye@163.com>
2024-02-09 00:07:28 +0800

    Initial commit

---
+++ db/migration/000001_init_schema.up.sql
@@ -0,0 +1,39 @@
+CREATE TABLE `account` (
+  `id` bigint PRIMARY KEY AUTO_INCREMENT,
+  `account_name` varchar(255) NOT NULL,
+  `balance` bigint NOT NULL,
+  `currency` varchar(255) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE `users` (
+  `id` bigint PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `account_id` bigint NOT NULL,
+  `amount` bigint NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE `transfers` (
+  `id` bigint PRIMARY KEY AUTO_INCREMENT,
+  `from_account_id` bigint NOT NULL,
+  `to_accout_id` bigint NOT NULL,
+  `amount` bigint NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX `account_index_0` ON `account` (`account_name`);
+
+CREATE INDEX `users_index_1` ON `users` (`account_id`);
+
+CREATE INDEX `transfers_index_2` ON `transfers` (`from_account_id`);
+
+CREATE INDEX `transfers_index_3` ON `transfers` (`to_accout_id`);
+
+CREATE INDEX `transfers_index_4` ON `transfers` (`from_account_id`, `to_accout_id`);
+
+ALTER TABLE `users` ADD FOREIGN KEY (`account_id`) REFERENCES `account` (`id`);
+
+ALTER TABLE `transfers` ADD FOREIGN KEY (`from_account_id`) REFERENCES `account` (`id`);
+
+ALTER TABLE `transfers` ADD FOREIGN KEY (`to_accout_id`) REFERENCES `account` (`id`)
---
+++ makefile
@@ -0,0 +1,7 @@
+mysql:
+	docker run --name mysqlbank -p 3307:3306 -e MYSQL_ROOT_PASSWORD=123456 -d mysql
+migrateup:
+	migrate  -path db/migration -database "mysql://root:123456@tcp(127.0.0.1:3307)/bank" -verbose up
+migratedown:
+	migrate  -path db/migration -database "mysql://root:123456@tcp(127.0.0.1:3307)/bank" -verbose down
+.PHONY: mysql migrateup migratedown
